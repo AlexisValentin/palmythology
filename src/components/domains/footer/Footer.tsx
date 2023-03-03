294055a7b75402db0cf0a02d49f92f1d71b95596
Alexis Valentin <alexis.valentin-ext@mappy.com>
2023-03-03 17:45:25 +0100

    [STYLES] add more tailwind responsive rules for footer

--- src/components/domains/footer/Footer.tsx
+++ src/components/domains/footer/Footer.tsx
@@ -3,7 +3,7 @@ import SocialNetworks from "../../generics/SocialNetworks";
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="grid grid-cols-2 fixed bottom-0 w-full text-white bg-black h-24 z-10 mt-20 px-6 md:px-20 xl:px-60">
+    <div className="grid grid-cols-2 fixed bottom-0 w-full text-white bg-black h-14 z-10 mt-20 px-6 sm:h-16 md:h-20 md:px-20 lg:h-24 xl:px-60">
       <div className="flex items-center justify-center text-sm px-6">
         <Copyrights />
       </div>
