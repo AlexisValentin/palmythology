9dbb69885fa47650a12b908aa27b8a1026d2b9e5
Alexis Valentin <alexis.jean.valentin@gmail.com>
2023-03-07 12:03:12 +0100

    [PACKAGES] get rid of unused dependancies

--- src/components/generics/PageSection.tsx
+++ src/components/generics/PageSection.tsx
@@ -11,35 +11,31 @@ const PageSection: React.FC<PageSectionProps> = ({
   gradient,
   iconUrl,
 }): JSX.Element => (
-  <>
-    <Link to={url} className="flex flex-row m-10 sm:block sm:w-full sm:m-0">
-      <section
-        className={`flex flex-col items-center rounded-3xl p-2.5 sm:rounded-none sm:flex-row ${
-          gradient
-            ? `bg-gradient-to-r from-${gradient?.startingColor} to-${gradient?.endingColor}`
-            : `bg-black text-white`
-        } sm:p-0`}
+  <Link to={url} className="flex flex-row m-5 sm:block sm:w-full sm:m-0">
+    <section
+      className={`flex flex-col items-center rounded-3xl p-2.5 sm:rounded-none sm:flex-row ${
+        gradient
+          ? `bg-gradient-to-r from-${gradient?.startingColor} to-${gradient?.endingColor}`
+          : `bg-black text-white`
+      } sm:p-0`}
+    >
+      <img
+        className="w-24 m-6 sm:m-12"
+        src={iconUrl}
+        alt={`${name} - ${description}`}
+      />
+      <div
+        className={`flex items-center grow ${
+          gradient && `text-white`
+        } sm:my-12 sm:mr-12`}
       >
-        <img
-          className="w-24 m-6 sm:m-12"
-          src={iconUrl}
-          alt={`${name} - ${description}`}
-        />
-        <div
-          className={`flex items-center grow ${
-            gradient && `text-white`
-          } sm:my-12 sm:mr-12`}
-        >
-          <div className="flex flex-col mt-2 mb-2">
-            <h2 className="font-semibold text-xl">{name}</h2>
-            <div className="font-medium mt-6 hidden md:block">
-              {description}
-            </div>
-          </div>
+        <div className="flex flex-col mt-2 mb-2">
+          <h2 className="font-semibold text-xl">{name}</h2>
+          <div className="font-medium mt-6 hidden md:block">{description}</div>
         </div>
-      </section>
-    </Link>
-  </>
+      </div>
+    </section>
+  </Link>
 );
 
 export default PageSection;
