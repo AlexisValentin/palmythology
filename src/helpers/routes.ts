91771c3130ae1fbb9dd6876ff7249df6977944ce
Alexis Valentin <alexis.valentin-ext@mappy.com>
2023-05-22 10:13:32 +0200

    [CARDS] handle better url parsing for slugs

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -1,11 +1,14 @@
 import { ROUTE_URLS } from "../types/consts/routes";
-import { stripDiacritics } from "./string";
+import { parseUrlToMatchSlug } from "./string";
 
 export const setCardRouteParameters = (cardName: string, pantheon: string) =>
   ROUTE_URLS.CARD.replace(":pantheon", pantheon).replace(
     ":card",
-    stripDiacritics(cardName.toLowerCase())
+    parseUrlToMatchSlug(cardName.toLowerCase())
   );
 
 export const setNewsRouteParameters = (newsName: string) =>
-  ROUTE_URLS.ARTICLE.replace(":title", stripDiacritics(newsName.toLowerCase()));
+  ROUTE_URLS.ARTICLE.replace(
+    ":title",
+    parseUrlToMatchSlug(newsName.toLowerCase())
+  );
--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,2 +1,8 @@
-export const stripDiacritics = (stringToStrip: string): string =>
+const stripDiacritics = (stringToStrip: string): string =>
   stringToStrip?.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+const replaceSpacesByDashes = (stringToParse: string): string =>
+  stringToParse.replace(/\s+/g, "-");
+
+export const parseUrlToMatchSlug = (stringToParse: string): string =>
+  replaceSpacesByDashes(stripDiacritics(stringToParse));
