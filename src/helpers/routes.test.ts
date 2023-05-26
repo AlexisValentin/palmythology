26f6ab6c1e26bc04cc187d7dee704d5f5b052eb4
Alexis Valentin <alexis.valentin-ext@mappy.com>
2023-05-26 15:03:44 +0200

    [TESTS] add unit tests for `helpers/routes`

---
+++ src/helpers/routes.test.ts
@@ -0,0 +1,34 @@
+import { setCardRouteParameters, setNewsRouteParameters } from "./routes";
+import { parseStringToSlug } from "./string";
+
+vi.mock("./string");
+
+describe("helpers/route", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe("setCardRouteParameters", () => {
+    test("should call `parseStringToSlug` method", () => {
+      const replaceSpy = vi.spyOn(String.prototype, "replace");
+
+      setCardRouteParameters("Jupiter", "Roman");
+      expect(replaceSpy).toHaveBeenCalledTimes(2);
+      expect(parseStringToSlug).toHaveBeenCalledTimes(1);
+      expect(parseStringToSlug).toHaveBeenCalledWith("jupiter");
+    });
+  });
+
+  describe("setNewsRouteParameters", () => {
+    test("should call `parseStringToSlug` method", () => {
+      const replaceSpy = vi.spyOn(String.prototype, "replace");
+
+      setNewsRouteParameters("Le guide de la Palmythology");
+      expect(replaceSpy).toHaveBeenCalledTimes(1);
+      expect(parseStringToSlug).toHaveBeenCalledTimes(1);
+      expect(parseStringToSlug).toHaveBeenCalledWith(
+        "le guide de la palmythology"
+      );
+    });
+  });
+});
