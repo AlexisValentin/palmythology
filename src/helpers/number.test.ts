5c3742d45414f9e2e1e0be3be9081fbcf6b49e4f
Alexis Valentin <alexis.valentin-ext@mappy.com>
2023-05-26 14:30:45 +0200

    [TESTS] add unit tests for `helpers/number`

---
+++ src/helpers/number.test.ts
@@ -0,0 +1,31 @@
+import { isEven, isOdd } from "./number";
+
+describe("helpers/number", () => {
+  describe("isEven", () => {
+    test("should return `true` for even numbers", () => {
+      for (let i = 0; i <= 10; i = i + 2) {
+        expect(isEven(i)).toEqual(true);
+      }
+    });
+
+    test("should return `false` for odd numbers", () => {
+      for (let i = 1; i <= 10; i = i + 2) {
+        expect(isEven(i)).toEqual(false);
+      }
+    });
+  });
+
+  describe("isOdd", () => {
+    test("should return `true` for odd numbers", () => {
+      for (let i = 1; i <= 10; i = i + 2) {
+        expect(isOdd(i)).toEqual(true);
+      }
+    });
+
+    test("should return `false` for even numbers", () => {
+      for (let i = 0; i <= 10; i = i + 2) {
+        expect(isOdd(i)).toEqual(false);
+      }
+    });
+  });
+});
