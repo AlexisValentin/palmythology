a510c722b1046baa0eb62049a28f4dec064b48f2
Alexis Valentin <alexis.valentin-ext@mappy.com>
2023-10-12 13:15:02 +0200

    [SEO] add small changes

--- src/wording/fr/seo.ts
+++ src/wording/fr/seo.ts
@@ -3,7 +3,7 @@ type SeoWordingType = { title: string; description: string }
 export const SEO_WORDING: Record<string, SeoWordingType> = {
   HOME: {
     title: `Page d'accueil`,
-    description: `Bienvenue sur le site de la Palmythology ! Découvrez les mythologies du monde en plongeant dans l'histoire et la culture divine à travers des fiches simple, intuitives et pédagogiques. Que vous souhaitiez en savoir plus sur les dieux, déesses, héros légendaires ou créatures mythiques, la Palmythology saura vous transporter dans une aventure éducative et divertissante !`,
+    description: `Découvrez les mythologies du monde en plongeant dans l'histoire et la culture divine à travers des fiches simple, intuitives et pédagogiques. Que vous souhaitiez en savoir plus sur les dieux, déesses, héros légendaires ou créatures mythiques, la Palmythology saura vous transporter dans une aventure éducative et divertissante !`,
   },
   NEWS: {
     title: 'Actualités',
