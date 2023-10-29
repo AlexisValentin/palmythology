d5917edf639e44b3a861eecdce998710707fc632
Alexis Valentin <alexis.jean.valentin@gmail.com>
2023-10-29 21:38:51 +0100

    [PANTHEONS] update pantheon icon links

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
-# Version 1.6.0 (??/??/2023)
+# Version 1.6.0 (29/10/2023)
 
 ## Général
 
+- Les couleurs et icônes des rubriques de la page d'accueil ont été modifiées afin de mieux harmoniser l'ensemble visuel.
 - Création d'une nouvelle rubrique "Panthéons" qui a pour but 1er de faciliter le référencement naturel du site, mais également d'orienter l'utilisateur quant aux différents contenus disponibles concernant certains panthéons bien précis.
 - La rubrique "Quoi 2 Neuf ?" est désormais accessible directement depuis la page d'accueil.
 - La rubrique "Dossiers" n'est désormais plus accessible.
@@ -11,6 +12,7 @@
 
 - La suggestion de fiches en lien avec celle actuellement consultée est à nouveau disponible sur écrans mobiles !
 - Le style des blocs de résumé a été modifié pour avoir des bordures rondes et une légère ombre projetée.
+- Correction d'un bug qui faisait que le bloc de recommandations de fiches similaires cassait l'aspect responsive sur les pages à au moins 3 recommandations.
 
 ---
 
--- src/types/cards/pantheons.ts
+++ src/types/cards/pantheons.ts
@@ -36,56 +36,51 @@ export const ALL_PANTHEON: PantheonSelectType[] = [
   {
     value: PantheonValue.AZTEC,
     label: PantheonLabel.AZTEC,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/150x150/3548952805/aztec.svg',
   },
   {
     value: PantheonValue.CELTIC,
     label: PantheonLabel.CELTIC,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/2808959fe9/celtic.svg',
   },
   {
     value: PantheonValue.CHINESE,
     label: PantheonLabel.CHINESE,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/150x150/a19a5de071/chinese.svg',
   },
   {
     value: PantheonValue.EGYPTIAN,
     label: PantheonLabel.EGYPTIAN,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/130f640778/egyptian.svg',
   },
   {
     value: PantheonValue.GREEK,
     label: PantheonLabel.GREEK,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/e84b55a294/greek.svg',
   },
   {
     value: PantheonValue.HINDU,
     label: PantheonLabel.HINDU,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/00f4254da4/hindu.svg',
   },
   {
     value: PantheonValue.JAPANESE,
     label: PantheonLabel.JAPANESE,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/150x150/e51cd3daed/japanese.svg',
   },
   {
     value: PantheonValue.MAYAN,
     label: PantheonLabel.MAYAN,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
-  },
-  {
-    value: PantheonValue.MESOPOTAMIAN,
-    label: PantheonLabel.MESOPOTAMIAN,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/41bbaee10a/mayan.svg',
   },
   {
     value: PantheonValue.NORSE,
     label: PantheonLabel.NORSE,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/512x512/608f27b0b1/norse.svg',
   },
   {
     value: PantheonValue.ROMAN,
     label: PantheonLabel.ROMAN,
-    icon: 'https://a.storyblok.com/f/187414/683x683/e80eb1688c/anubis.svg',
+    icon: 'https://a.storyblok.com/f/187414/683x683/c30288bcb8/roman.svg',
   },
 ]
