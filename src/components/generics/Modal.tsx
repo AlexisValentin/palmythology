3a9d2a2e19703696d067fc830f0b5fa7ea7130bf
Alexis Valentin <alexis.valentin-ext@mappy.com>
2024-07-07 22:57:28 +0200

    fix: adapate tracking notice on small devices

--- src/components/generics/Modal.tsx
+++ src/components/generics/Modal.tsx
@@ -12,7 +12,7 @@ const Modal: React.FC<ModalProps> = ({ modalId, onClick }) => {
   const { label, color } = button
 
   return (
-    <div className="fixed bg-white top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 shadow-2xl rounded-lg">
+    <div className="fixed bg-white border-t-2 border-slate-300 bottom-0 w-full md:border-0 md:w-auto md:bottom-auto md:top-1/2 md:left-1/2 z-50 md:-translate-x-1/2 md:-translate-y-1/2 shadow-2xl rounded-lg">
       <div className="m-8">
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         <div className="text-sm mb-4">{description}</div>
