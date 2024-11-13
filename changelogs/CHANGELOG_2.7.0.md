4cfa17df0c6c9c1b5071167a6b5ed996a1d892a0
Alexis Valentin <alexis.jean.valentin@gmail.com>
2024-11-13 17:27:58 +0100

    feat: include last and antepenultimate months for q2n schedule

--- changelogs/CHANGELOG_2.7.0.md
+++ changelogs/CHANGELOG_2.7.0.md
@@ -4,6 +4,10 @@
 
 - Ajout d'une section en page d'accueil générant aléatoirement le descriptif d'une fiche "Les grandes lignes", afin de mettre en avant de manière égale l'ensemble des fiches du site Internet.
 
+# Quoi 2 Neuf
+
+- Il est désormais possible de visualiser le planning des 2 derniers mois en plus de celui du mois actuel, dans le but de pouvoir consulter une archive des fiches les plus récemment publiées sur le site web.
+
 ## Divers
 
 - Désactivation du lien en pied de page vers `tipeee.com`.
--- src/components/generics/PageHeader.tsx
+++ src/components/generics/PageHeader.tsx
@@ -2,19 +2,21 @@ interface PageHeaderProps {
   title: string
   subtitle?: string
   upperGap?: boolean
+  fullDisplay?: boolean
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({
   title,
   subtitle,
   upperGap = true,
+  fullDisplay: forceDisplay = false,
 }): JSX.Element => (
   <div
     className={`flex flex-col items-center justify-center ${upperGap ? 'mt-12 mb-6' : 'my-6'}`}
   >
     <h1 className=" text-2xl font-bold">{title}</h1>
     {subtitle && (
-      <div className="hidden md:block">
+      <div className={`${forceDisplay ? 'block' : 'hidden'} md:block`}>
         <h2 className="italic text-xs text-nowrap sm:block md:text-sm lg:text-base pt-2">
           {subtitle}
         </h2>
