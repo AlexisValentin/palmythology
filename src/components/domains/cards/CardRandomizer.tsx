5225a96c341820c6c5d65b409898ee9ef8189c8d
Alexis Valentin <alexis.jean.valentin@gmail.com>
2024-11-18 15:54:54 +0100

    fix: update card randomizer responsive behaviour

--- src/components/domains/cards/CardRandomizer.tsx
+++ src/components/domains/cards/CardRandomizer.tsx
@@ -25,22 +25,22 @@ const CardRandomizer: React.FC = async () => {
       </h3>
       <Link
         href={getRandomCardUrl()}
-        className="group flex justify-center flex-col md:flex-row items-center mt-4 border border-black rounded-lg overflow-hidden hover:bg-black"
+        className="group flex justify-center flex-col lg:flex-row items-center mt-4 lg:border lg:border-black rounded-lg overflow-hidden hover:bg-black"
       >
         <Image
+          className="basis-1/3"
           src={images[0].filename}
           alt={`Couverture de la fiche ${name} du panthéon ${pantheon}`}
           width={350}
           height={350}
         />
-
-        <div className="m-4 max-h-80">
-          <div className="hidden md:block mb-4">
+        <div className="m-4 max-h-80 basis-2/3">
+          <div className="hidden lg:block mb-4">
             <h4 className="text-xl font-bold group-hover:text-white">{name}</h4>
             <h5 className="font semibold group-hover:text-white">{subtitle}</h5>
           </div>
-          <div className="flex text-center md:text-left mt-4 md:mt-0">
-            <div className="italic max-w-80 md:max-w-96 group-hover:text-white text-ellipsis overflow-hidden">
+          <div className="flex text-center lg:text-left mt-4 md:mt-0">
+            <div className="italic max-w-80 md:max-w-fit group-hover:text-white text-ellipsis overflow-hidden">
               <p className="text-ellipsis overflow-hidden">{summary}</p>
             </div>
           </div>
