1d096326ec217170d9109924195e79b0af747664
Alexis Valentin <alexis.valentin-ext@mappy.com>
2025-03-21 12:18:53 +0100

    refactor: improve card transcription by using a dedicated `Faq` component

---
+++ src/components/domains/cards/Transcription.tsx
@@ -0,0 +1,41 @@
+import Faq from '../../generics/Faq'
+
+interface TranscriptionDetailContent {
+  transcriptionDetailTitle: string
+  transcriptionDetailContent: string
+}
+
+interface TranscriptionContent {
+  transcriptionTitle: string
+  transcriptionContent: TranscriptionDetailContent[]
+}
+
+interface TranscriptionProps {
+  transcription: TranscriptionContent[]
+}
+
+const Transcription: React.FC<TranscriptionProps> = ({ transcription }) => (
+  <div className="mt-16 w-full lg:w-3/4">
+    {transcription.map((faq) => {
+      const { transcriptionTitle, transcriptionContent } = faq
+
+      // workaround to avoid typescripting issue and in order to comply to `Faq` component
+      // should be fixed as soon as possible by creating a `Faq` block in the backend
+      const parsedTranscriptionContent = transcriptionContent.map((item) => ({
+        title: item.transcriptionDetailTitle,
+        description: item.transcriptionDetailContent,
+      }))
+
+      return (
+        <div className="mb-8" key={transcriptionTitle}>
+          <Faq
+            faqTitle={transcriptionTitle}
+            faqItems={parsedTranscriptionContent}
+          />
+        </div>
+      )
+    })}
+  </div>
+)
+
+export default Transcription
