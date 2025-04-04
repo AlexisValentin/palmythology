d27a6da7a9fbb46d878e85597caa5ad0b32417be
Alexis Valentin <alexis.jean.valentin@gmail.com>
2025-04-04 23:33:10 +0200

    feat: introduce react testing library

--- src/components/generics/Button.tsx
+++ src/components/generics/Button.tsx
@@ -5,18 +5,16 @@ export interface ButtonProps {
   icon?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ label, color, onClick, icon }) => {
-  return (
-    <button
-      className={`bg-${color} text-white rounded-lg hover:opacity-75`}
-      onClick={onClick}
-    >
-      <div className="m-2">
-        {icon && <span>icon</span>}
-        <span>{label}</span>
-      </div>
-    </button>
-  )
-}
+const Button: React.FC<ButtonProps> = ({ label, color, onClick, icon }) => (
+  <button
+    className={`bg-${color} text-white rounded-lg hover:opacity-75`}
+    onClick={onClick}
+  >
+    <div className="m-2">
+      {icon && <span>{icon}</span>}
+      <span>{label}</span>
+    </div>
+  </button>
+)
 
 export default Button
