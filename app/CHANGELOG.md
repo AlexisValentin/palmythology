aba8493cd65771344b62b98bb5da450503d0566d
a.valentin <a.valentin@matchem.fr>
2025-10-27 16:12:05 +0100

    build: upgrade NextJS to major 16.0.0

---
+++ app/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Notes de version 2.15.0 (??/10/2025)
+
+# Général
+
+- Mise à jour de Next JS vers la version majeure 16.0.0.
--- next.config.js
+++ next.config.js
@@ -4,21 +4,6 @@ const nextConfig = {
 	images: {
 		remotePatterns: [{ protocol: "https", hostname: "a.storyblok.com" }],
 	},
-	webpack: (config, { webpack }) => {
-		config.experiments = { ...config.experiments, topLevelAwait: true };
-		config.externals["node:fs"] = "commonjs node:fs";
-		config.resolve.fallback = {
-			...config.resolve.fallback,
-			fs: false,
-		};
-		config.plugins.push(
-			new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-				resource.request = resource.request.replace(/^node:/, "");
-			}),
-		);
-
-		return config;
-	},
 	env: {
 		STORYBLOK_TOKEN: process.env.STORYBLOK_TOKEN,
 		STORYBLOK_BASE_URL: process.env.STORYBLOK_BASE_URL,
