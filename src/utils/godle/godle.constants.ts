9ce7c6b36330b57aa3d6ce3f4cd634ef6066efee
Alexis Valentin <alexis.jean.valentin@gmail.com>
2026-01-23 16:47:56 +0100

    fix: update godle epoch

--- src/utils/godle/godle.constants.ts
+++ src/utils/godle/godle.constants.ts
@@ -1,5 +1,5 @@
 export const GODLE_CONFIG = {
-	EPOCH_START: new Date("2026-01-09").getTime(),
+	EPOCH_START: new Date("2026-01-23").getTime(),
 	GAME_NAME: "Godle",
 	SHARE_URL: "https://palmythology.com/godle",
 };
